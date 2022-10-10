1060e3dcfef4977a8964431184bf8f818d9e778c
Eric02851 <ziggy2851@gmail.com>
2022-10-10 13:52:14 -0700

    Add sublist select all and deselect all buttons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import awl from './AWL.json' assert {type: 'json'}
+const sublists = document.querySelectorAll('input[type="checkbox"]')
 
-const main = () => {
-    const sublists = document.querySelectorAll('input[type="checkbox"]')
+const searchPaper = () => {
+    const wordCount = document.getElementById('wordCount')
+    const wordsOut = document.getElementById('wordsFound')
+    let paper = document.getElementById('paper')
+
+    paper = paper.value.trim().toLowerCase()
+    paper = paper.replace(/[^A-Za-z\s]/g, '')
+    paper = paper.split(/\s+/)
+
+    if (paper.length == 1 && paper[0] == '') {
+        wordsOut.textContent = ""
+        wordCount.style = 'color:red'
+        wordCount.textContent = ' (Paper Not Entered)'
+        return
+    }
+
     let checked = []
-    for (let i of sublists) {
-        if (i.checked)
-            checked.push(i.value)
+    for (let i of sublists)
+        if (i.checked) checked.push(i.value)
+
+    if (checked.length == 0) {
+        wordsOut.textContent = ""
+        wordCount.style = 'color:red'
+        wordCount.textContent = ' (No Sublists Selected)'
+        return
     }
 
     let lookupDict = {}
@@ -15,24 +35,24 @@ const main = () => {
         for (let j of awl[i].pastTense) lookupDict[j] = true
     }
 
-    let paper = document.getElementById("paper").value.trim().toLowerCase()
-    paper = paper.replace(/[^A-Za-z\s]/g, '')
-    paper = paper.split(/\s+/)
-
     let wordsFound = []
-    for (let i of paper)
-        if (lookupDict[i]) wordsFound.push(i)
+    for (let i of paper) if (lookupDict[i]) wordsFound.push(i)
     wordsFound = Array.from(new Set(wordsFound))
 
-    let wordStr = ""
-    for (let i of wordsFound)
-        wordStr += i + ', '
-
+    let wordStr = ''
+    for (let i of wordsFound) wordStr += i + ', '
     wordStr = wordStr.slice(0, wordStr.length - 2)
-    wordStr += ` (${wordsFound.length})`
-    document.getElementById("wordsFound").textContent = wordStr
+    wordsOut.textContent = wordStr
 
+    if (wordsFound.length != 0) wordCount.style = 'color:green;'
+    else wordCount.style = 'color:red'
+    wordCount.textContent = ` (${wordsFound.length})`
+
+    console.log(lookupDict)
+    console.log(paper)
     console.log(wordsFound)
 }
 
-document.querySelector('input[type="button"]').addEventListener('click', main)
+document.getElementById('selectButton').addEventListener('click', function() {for (let i of sublists) i.checked = true})
+document.getElementById('deselectButton').addEventListener('click', function() {for (let i of sublists) i.checked = false})
+document.getElementById('searchButton').addEventListener('click', searchPaper)
